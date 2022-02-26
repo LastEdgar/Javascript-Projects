d1bb177e3d3f35181e0c49dbaca3cf92cd0a354b
LastEdgar <david.a.edgar96@gmail.com>
2022-02-26 16:46:34 -0500

    Function Tests
    Using button elements and function calls with JS

---
+++ Basic Javascript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,5 @@
+function assignVar() {
+    var words = "Was gonna be math but now it isn\'t";
+    var result = words.fontcolor("red");
+    document.getElementById("redWords").innerHTML = result;
+}
