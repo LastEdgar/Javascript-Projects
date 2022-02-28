55da5eaf047fa2b2e5f978af3df6ab312a02d5cd
LastEdgar <david.a.edgar96@gmail.com>
2022-02-28 18:25:29 -0500

    Assignment Submission 2
    Concatenating string test

--- Basic Javascript Projects/Project2_functions/JS/main.js
+++ Basic Javascript Projects/Project2_functions/JS/main.js
@@ -1,5 +1,6 @@
 function assignVar() {
     var words = "Was gonna be math but now it isn\'t";
+    words += " and now it has more added to it";
     var result = words.fontcolor("red");
     document.getElementById("redWords").innerHTML = result;
 }
