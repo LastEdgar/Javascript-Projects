3b5f25c04e55a61b0db22ec737acf2cb0582c48d
LastEdgar <david.a.edgar96@gmail.com>
2022-03-01 22:59:06 -0500

    Project folder 3
    Test of  functions, events, basic calculations and getElementById

---
+++ Basic Javascript Projects/Project3_math_operations/JS/main.js
@@ -0,0 +1,19 @@
+function addition() {
+    var adder = 10 + 5;
+    document.getElementById("Math").innerHTML = "Ten plus five is... " + adder;
+}
+
+function subtract() {
+    var minus = 10 - 5;
+    document.getElementById("Math2").innerHTML = "but ten minus five is... " + minus;
+}
+
+function multiply() {
+    var multi = 10 * 5;
+    document.getElementById("Math3").innerHTML = "Ten times five is... " + multi;
+}
+
+function divide() {
+    var divi = 10 / 5;
+    document.getElementById("Math4").innerHTML = "Ten divided by five is... " + divi;
+}
