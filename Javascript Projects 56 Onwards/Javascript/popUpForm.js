ab5b0e4848d9c30a6ea8a3ff9e67d734b1f2851e
LastEdgar <david.a.edgar96@gmail.com>
2022-03-25 12:37:19 -0400

    React, toDo App, challenge work
    JS challenges including autoslideshow, sticky elements, pop-up forms, animations, React tasks

---
+++ Javascript Projects 56 Onwards/Javascript/popUpForm.js
@@ -0,0 +1,7 @@
+function openForm() {
+    document.getElementById("theForm").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("theForm").style.display = "none";
+}
